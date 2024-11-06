5383f85a1edc94e91cb26f65cfbd5f24c8b90fea
Jack Sebben <jacksebben11@hotmail.com>
2024-11-05 21:41:56 -0500

    Added some tests using Minitest and Rack to test different routes.

--- Gemfile
+++ Gemfile
@@ -2,11 +2,17 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sinatra-contrib"
-gem "erubis"
+gem "erubis"  
 gem "webrick"
+gem "redcarpet"
 
 ruby "3.2.2"
 
 group :production do
   gem "puma"
+end
+
+group :test do
+  gem "minitest"
+  gem "rack-test"
 end
--- cms.rb
+++ cms.rb
@@ -3,22 +3,28 @@ require "sinatra/content_for"
 require "tilt/erubis"
 require "rack"
 require "json"
+require "redcarpet"
+
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(64)
+end
 
 before do 
-  @root = File.expand_path("..", __FILE__)
+  @root  = File.expand_path("..", __FILE__)
   @files = Dir.children('data')
+  @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 end
 
 helpers do
   def remove_ext(file_name)
     File.basename(file_name, ".*")
-  end
+  end 
 end
 
-
-
 get '/' do
-  erb :home, layout: :layout
+  erb :index, layout: :layout
 end
 
 get '/:file_name' do
@@ -28,6 +34,7 @@ get '/:file_name' do
     headers["Content-Type"] = "text/plain"
     File.read("data/#{@file_name}")
   else
+    session[:error] = "#{@file_name} does not exist."
     redirect '/'
   end
 end
